2a8a66d8a2071a0d2bad384749279bae51e33438
sujeevanferos <ferosraveendransujeevan@gmail.com>
2025-04-29 09:39:43 +0530

    nothing

---
+++ simplePrograms/reverse_algorithm.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+
+    /* Write the logic to reverse the array. */
+    int start = 0, end = num -1;
+    while(start < end){
+        int temp  = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        
+        start++;
+        end--;
+    }
+
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    
+    free(arr);
+    return 0;
+}
